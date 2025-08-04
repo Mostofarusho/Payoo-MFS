e2dc2a6862f507d732a4cbdf369699a8972a01c6
MostofaFattha <mostofacse14@gmail.com>
2025-08-04 11:24:43 +0600

    handle-login-and-redirect-to-the-home-page

--- js/login.js
+++ js/login.js
@@ -7,5 +7,11 @@ document.getElementById('button-login').addEventListener('click', (event) => {
     const phoneNumber = document.getElementById('phone-number').value;
     const userPassword = document.getElementById('user-password').value;
     console.log(phoneNumber, userPassword);
+    if (phoneNumber === '5' && userPassword === '1234') {
+        console.log('You are Logged In');
+        window.location.href = 'home.html'
+    } else {
+        alert('Invalid Input');
+    }
 
 })
