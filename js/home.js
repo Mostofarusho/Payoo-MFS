28fab8fb51e66e479e578a55b7ff37d783a0815e
MostofaFattha <mostofacse14@gmail.com>
2025-08-05 08:37:55 +0600

    verify-pin-number-and-add-money-to-the-balance

--- js/home.js
+++ js/home.js
@@ -1,9 +1,24 @@
-// Add Money To The Account
-// Step 1  
 document.getElementById('button-add-money').addEventListener('click', (event) => {
     event.preventDefault();
-    console.log('Money Add Button Clicked');
+
     const addedMoneyInput = document.getElementById('add-money-input-field').value;
     const addedMoneyPasswordInput = document.getElementById('add-money-password-input-field').value;
 
-})
+    if (addedMoneyPasswordInput === '1234') {
+        const accountBalance = document.getElementById('account-balance');
+        const currentBalance = parseFloat(accountBalance.innerText);
+        const newInputMoney = parseFloat(addedMoneyInput);
+
+        if (!isNaN(newInputMoney)) {
+            const newBalance = currentBalance + newInputMoney;
+            accountBalance.innerText = newBalance.toFixed(2);
+            console.log('New Balance:', newBalance);
+            alert('Money added successfully!');
+        } else {
+            alert('Invalid amount entered');
+        }
+
+    } else {
+        alert('Failed To add Money: Wrong Password');
+    }
+});
