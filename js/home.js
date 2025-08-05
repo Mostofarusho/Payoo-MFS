21074b5efadf01494491a0953614acf064c956d7
MostofaFattha <mostofacse14@gmail.com>
2025-08-05 09:32:45 +0600

    add-money-to-the-balance-after-pin-verification

--- js/home.js
+++ js/home.js
@@ -14,6 +14,8 @@ document.getElementById('button-add-money').addEventListener('click', (event) =>
             accountBalance.innerText = newBalance.toFixed(2);
             console.log('New Balance:', newBalance);
             alert('Money added successfully!');
+            document.getElementById('add-money-input-field').value = '';
+            document.getElementById('add-money-password-input-field').value = '';
         } else {
             alert('Invalid amount entered');
         }
@@ -22,3 +24,30 @@ document.getElementById('button-add-money').addEventListener('click', (event) =>
         alert('Failed To add Money: Wrong Password');
     }
 });
+
+document.getElementById('button-cash-out-money').addEventListener('click', () => {
+    const reduceMoneyInput = document.getElementById('add-money-input-field').value;
+    const reduceMoneyPasswordInput = document.getElementById('add-money-password-input-field').value;
+
+    if (reduceMoneyPasswordInput === '1234') {
+        const accountBalance = document.getElementById('account-balance');
+        const currentBalance = parseFloat(accountBalance.innerText);
+        const newInputMoney = parseFloat(reduceMoneyInput);
+        if (!isNaN(newInputMoney)) {
+            if (newInputMoney > currentBalance) {
+                alert("Insufficient balance.");
+                return;
+            }
+            const newBalance = currentBalance - newInputMoney;
+            accountBalance.innerText = newBalance.toFixed(2);
+            console.log('Reduce Money', accountBalance);
+            alert('Money Reduce Successfully')
+            document.getElementById('add-money-input-field').value = '';
+            document.getElementById('add-money-password-input-field').value = '';
+        } else {
+            alert('Invalid Amount Entered');
+        }
+    } else {
+        alert('Failed To Reduce Money: Wrong Password');
+    }
+})
