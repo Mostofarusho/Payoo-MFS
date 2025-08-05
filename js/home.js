f42f0d1259d4c668e3136b50fefd61c63681788a
MostofaFattha <mostofacse14@gmail.com>
2025-08-05 07:54:07 +0600

    handle-add-money-event-handler-on-the-button-click

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-console.log("hello");
+// Add Money To The Account
+// Step 1  
+document.getElementById('button-add-money').addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('Money Add Button Clicked');
+    const addedMoneyInput = document.getElementById('add-money-input-field').value;
+    const addedMoneyPasswordInput = document.getElementById('add-money-password-input-field').value;
+
+})
